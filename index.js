b05aab151a87a3b3bbda1a104c867cf25404d49d
Rachel Acosta <denversunrise@gmail.com>
2017-12-13 16:08:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, object[key] = value);
+  var newObj = Object.assign({}, object, {[key]:value});
+  return newObj;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  return {object, [key]: value};
+  return Object.assign({object, [key]: value});
 }
 
 function deleteFromObjectByKey(object, key){
